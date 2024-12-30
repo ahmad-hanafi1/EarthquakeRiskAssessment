abd7a026f935914216dd4dd5510ef322ec43baad
ahmad-hanafi1 <ahmadhanafi235@gmail.com>
2024-12-30 10:05:20 +0100

    finished save button

--- src/Page5.jsx
+++ src/Page5.jsx
@@ -21,45 +21,49 @@ const Page5 = () => {
   return (
     <>
       <div className="container" id="container1">
-        <h1 style={{ width: "100%", minWidth: "400px" }}>Result</h1>
-        {data?.image ? (
-          <img
-            className="image"
-            alt="not found"
-            width={"250px"}
-            src={data?.image ? URL.createObjectURL(data?.image) : ""}
-          />
-        ) : null}
+        <div>
+          <h1 style={{ width: "100%", minWidth: "400px" }}>Result</h1>
+          {data?.image ? (
+            <img
+              className="image"
+              alt="not found"
+              width={"250px"}
+              src={data?.image ? URL.createObjectURL(data?.image) : ""}
+            />
+          ) : null}
+          <p>
+            Building ID: <span style={{ fontWeight: "600" }}>{data?.name}</span>
+          </p>
+          <p>
+            Building Address:{" "}
+            <span style={{ fontWeight: "600" }}>{data?.address}</span>
+          </p>
+          <p>
+            Date: <span style={{ fontWeight: "600" }}>{data?.date}</span>
+          </p>
+          <p>
+            Earthquake Risk Score:{" "}
+            <span style={{ fontWeight: "600" }}>
+              {data?.earthquakeRiskScore}
+            </span>
+          </p>
+          <span
+            style={{
+              padding: "15px 30px",
+              backgroundColor:
+                data?.riskStatus === "Low Risk" ||
+                data?.riskStatus === "No Risk"
+                  ? "green"
+                  : "red",
+              width: "fit",
+              color: "white",
+              borderRadius: "10px",
+            }}
+          >
+            {data?.riskStatus}
+          </span>
+        </div>
         <p>
-          Building ID: <span style={{ fontWeight: "600" }}>{data?.name}</span>
-        </p>
-        <p>
-          Building Address:{" "}
-          <span style={{ fontWeight: "600" }}>{data?.address}</span>
-        </p>
-        <p>
-          Date: <span style={{ fontWeight: "600" }}>{data?.date}</span>
-        </p>
-        <p>
-          Earthquake Risk Score:{" "}
-          <span style={{ fontWeight: "600" }}>{data?.earthquakeRiskScore}</span>
-        </p>
-        <span
-          style={{
-            padding: "15px 30px",
-            backgroundColor:
-              data?.riskStatus === "Low Risk" || data?.riskStatus === "No Risk"
-                ? "green"
-                : "red",
-            width: "fit",
-            color: "white",
-            borderRadius: "10px",
-          }}
-        >
-          {data?.riskStatus}
-        </span>
-        <p>
-          {" "}
           <Link to={"/"} className="backLink">
             Test again
           </Link>
